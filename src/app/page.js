4303db385a065b44bbeb3c54cabc5c6887d645be
Aakash <vermaaakash1000@gmail.com>
2024-04-06 23:10:32 +0530

    Hero section updated

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ const myFont = font({src : '../../public/fonts/MilkyNice.otf'})
 
 export default function Home() {
   return (
-    <main className={`${myFont.className} flex min-h-[90vh] h-[10vh] flex-col items-center justify-between `}>
+    <main className={`${myFont.className} flex flex-col items-center justify-between `}>
       <Hero/>
     </main>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,49 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
-import cartoon from '../../public/assets/cartoon.svg'
+import cartoon from '../../public/assets/cartoonUpdated.svg'
 import logo from '@/../public/assets/heroLogo.svg'
 import heroCloud from '@/../public/assets/heroCloud.svg'
+import heroLines from '@/../public/assets/heroLines.svg'
+import club1 from '@/../public/assets/club1.svg'
+import club2 from '@/../public/assets/club2.svg'
+import club3 from '@/../public/assets/club3.svg'
 const Hero = () => {
+    const clubArray = [club1, club2, club3]
     return (
-        <div className='bg-[#1692CD] h-4/5 w-full flex '>
-            <div className='w-1/2 flex flex-col justify-center  items-center  h-full relative'>
-                <Image src={logo} className='absolute border-4 box-content left-10 -top-14 border-white bg-white rounded-full z-10' />
-
-                <div className='w-3/4  self-end flex flex-col gap-4 mr-5 '>
-                    <p className='text-white text-5xl w-full  leading-normal self-end drop-shadow-[0_4px_35px_rgba(0,0,0,0.30)]'>
-                        Let's travel virtually by cooking food from around the world.
-                    </p>
-                    <button className='self-start text-white px-5 py-3 font-medium text-2xl bg-[#E6038A] rounded-full'>Let's Cokkin'</button>
+        <div className='h-[140vh] flex flex-col'>
+            <div className='bg-[#1692CD] h-[1/2] w-full flex '>
+                <div className='w-1/2 h-full flex flex-col  justify-center items-center relative'>
+                    <Image src={logo} className='absolute border-4 box-content md:left-28 left-14 -top-14 border-white bg-white rounded-full z-10' />
+                    <div className='w-3/4 self-end flex flex-col gap-4 mr-5 relative'>
+                        <p className='text-white text-5xl w-full  leading-normal self-end drop-shadow-[0_4px_35px_rgba(0,0,0,0.30)]'>
+                            Let's travel virtually by cooking food from around the world.
+                        </p>
+                        <button className='self-start text-white px-5 py-3 font-medium text-2xl bg-[#E6038A] rounded-full'>Let's Cokkin'</button>
+                        <Image src={heroLines} alt='lines' className='absolute -bottom-16 -left-14' />
+                    </div>
                 </div>
+                <div className='w-1/2  relative flex justify-center h-full'>
 
+                    <Image className='' src={cartoon} />
+                    <Image className='absolute right-0 top-7' src={heroCloud} />
+                </div>
             </div>
-            <div className='w-1/2  relative flex justify-center'>
-                <Image className='absolute right-0 top-7' src={heroCloud} />
-                <Image className='' src={cartoon} />
+            <div className='h-[1/2] pt-20 w-full flex flex-col justify-center items-center border border-black'>
+                <p className='text-[#40AE49] text-xl mb-4'>Let's Cook Together</p>
+                <h2 className='text-[#CC017A] text-5xl'>Which Club is Right for you</h2>
+                <div className='flex flex-row gap-8'>
+                    { 
+                        clubArray.map((val, index) => {
+                            return (
+                                <Image src={val} alt="Club SVG" />
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
+
     )
 }
 
