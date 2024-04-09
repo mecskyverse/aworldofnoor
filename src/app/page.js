1328df1a86c7f6e754eac210d8a35953329f6850
Aakash <vermaaakash1000@gmail.com>
2024-04-09 05:45:17 +0530

    How it works section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import font from 'next/font/local'
 import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
 const myFont = font({src : '../../public/fonts/MilkyNice.otf'})
 
 export default function Home() {
   return (
     <main className={`${myFont.className} flex flex-col items-center justify-between `}>
       <Hero/>
+      <HowItWorks/>
     </main>
   );
 }
