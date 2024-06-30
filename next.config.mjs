71b9cb61d83214b64a65b90bbd0eefa8cda1d3f5
Aakash <vermaaakash1000@gmail.com>
2024-06-30 18:44:21 +0530

    remote image hostname added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'pyixtvhlfinkbjiogvsm.supabase.co',
+            port: '',
+            pathname: '/storage/v1/object/public/user-images/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
