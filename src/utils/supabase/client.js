4980e11f279d6804e622467612e8e9574b4b0e61
Aakash <vermaaakash1000@gmail.com>
2024-06-30 18:49:49 +0530

    supabase clients and middlewares added

--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+const client = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 export function createNewClient() {
   // Create a supabase client on the browser with project's credentials
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  )
-}
+  return client
+} 
