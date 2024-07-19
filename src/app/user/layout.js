f43f811e98385cf52df567eeba0a1ab1276dfe8a
Aakash <vermaaakash1000@gmail.com>
2024-07-19 23:41:27 +0530

    user page router layout updated for user check

--- src/app/user/layout.js
+++ src/app/user/layout.js
@@ -1,10 +1,29 @@
+'use client'
+import {useEffect} from 'react'
 import Sidebar from "@/components/Sidebar"
+import { createNewClient } from '@/utils/supabase/client'
+import { useRouter } from 'next/navigation'
 
 export default function DashboardLayout({children}) {
+  const router = useRouter()
+    const supabase = createNewClient()
+    useEffect(() => {
+        const getUserDetails = async () => {
+            const {
+              data: { user },
+              error
+            } = await supabase.auth.getUser()
+            if (error || !user) {
+                router.push('/signin')
+             
+            }  
+        } 
+        getUserDetails()
+    }, [])
     return(
     <section className="flex items-start justify-between">
         <Sidebar />
-        <div className="p-8 grid w-full h-full pl-[300px]">
+        <div className="p-8 flex w-full h-full pl-[300px]">
             {children}
           </div>
     </section>
