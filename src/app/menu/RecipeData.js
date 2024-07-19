cdd9c8962e3653e79fb4cf548e0f2bc550e3ecea
Aakash <vermaaakash1000@gmail.com>
2024-07-19 23:40:41 +0530

    Recipe Data added

---
+++ src/app/menu/RecipeData.js
@@ -0,0 +1,63 @@
+import menu7Image from '@/../public/assets/cbackground.jpg'
+import menu8Image from '@/../public/assets/homeslide2.png'
+import menu9Image from '@/../public/assets/homeslide3.png'
+import menu10Image from '@/../public/assets/homeslide4.png'
+import menu11Image from '@/../public/assets/homeslide5.png'
+import menu12Image from '@/../public/assets/Homeslide6.png'
+
+export const dishes = [
+    {
+        id: 1,
+        name: 'CHILLI CON CARNE',
+        description: 'Indulge in the rich and savory symphony of flavors with our Savory Herb-Infused Chicken',
+        image: menu7Image,
+        time: '40 MIN',
+        difficulty: 'EASY PREP',
+        servings: '3 SERVES'
+    },
+    {
+        id: 2,
+        name: 'PENNE WITH SALMON and Basil & Tomatoes',
+        description: 'Experience the perfect blend of zesty lemon and aromatic garlic with this roasted chicken recipe',
+        image: menu8Image,
+        time: '1 HOUR',
+        difficulty: 'HARD PREP',
+        servings: '4 SERVES'
+    },
+    {
+        id: 3,
+        name: 'MATAR KHEEMA (Peas and Meat)',
+        description: 'Quick, wholesome, and bursting with flavors, it’s perfect for a healthy weeknight dinner.',
+        image: menu9Image,
+        time: '30 MIN',
+        difficulty: 'EASY PREP',
+        servings: '3 SERVES'
+    },
+    {
+        id: 4,
+        name: 'WIENER SCHNITZEL',
+        description: 'This berry smoothie bowl is not only visually appealing but also a powerhouse of antioxidants.',
+        image: menu10Image,
+        time: '10 MIN',
+        difficulty: 'EASY PREP',
+        servings: '2 SERVES'
+    },
+    {
+        id: 5,
+        name: 'Spaghetti Aglio e Olio',
+        description: 'A minimalist yet flavorful dish with garlic, olive oil, and a hint of red pepper flakes.',
+        image: menu11Image,
+        time: '20 MIN',
+        difficulty: 'EASY PREP',
+        servings: '2 SERVES'
+    },
+    {
+        id: 6,
+        name: 'Grilled Veggies with Sauce',
+        description: 'Served with a zesty chimichurri sauce it is a perfect addition to your outdoor gatherings.',
+        image: menu12Image,
+        time: '25 MIN',
+        difficulty: 'MEDIUM PREP',
+        servings: '6 SERVES'
+    },
+];
---
+++ src/app/menu/[id]/data.js
@@ -0,0 +1,203 @@
+// Import dish images
+import menu7Image from '@/../../public/assets/cbackground.jpg'
+import menu8Image from '@/../../public/assets/homeslide2.png'
+import menu9Image from '@/../../public/assets/homeslide3.png'
+import menu10Image from '@/../../public/assets/homeslide4.png'
+import menu11Image from '@/../../public/assets/homeslide5.png'
+import menu12Image from '@/../../public/assets/Homeslide6.png'
+
+export const dishes = [
+    {
+      id: 1,
+      name: 'CHILLI CON CARNE',
+      description: 'Indulge in the rich and savory symphony of flavors with our Savory Herb-Infused Chicken',
+      image: menu7Image.src,
+      time: '40 MIN',
+      difficulty: 'EASY PREP',
+      servings: '3 SERVES',
+      ingredients: [
+        '2 tbsp olive oil',
+        '1 large onion, diced',
+        '4 cloves garlic, chopped',
+        '2 lbs ground beef',
+        '2 tbsp ground cumin',
+        '2 tbsp dried oregano',
+        '2 tbsp paprika',
+        '1/2 tsp cayenne pepper (optional)',
+        '2 tbsp tomato paste',
+        '1 can (28oz) crushed or diced tomatoes',
+        '1/2 cup beef stock',
+        '1 can kidney beans, drained and rinsed',
+        'Optional: 1/2 cup corn kernels',
+        'Optional: 2 carrots, chopped'
+      ],
+      prepHacks: [
+        'Cook Steps 1 and 2 in advance.',
+        'Simulate steps with no heat and simmer at the end.',
+      ],
+      NoorTips:[
+         'For a bit of a spicy kick, add tabasco sauce or cayenne pepper along with the other spices. Top with lots of sour cream, chopped green onions and grated cheese.',
+         'Adventurer Tip:',
+         'Make bread bowls to serve in! Buy an uncut loaf and hollow it out, or use smaller individual ones.',
+      ],
+      instructions: [
+        '1. Heat olive oil in a large pot over medium heat. Add diced onions and sauté until translucent.',
+        '2. Add chopped garlic and cook for another minute.',
+         '3. Add ground beef to the pot and cook, stirring occasionally, until browned.',
+        '4. Stir in ground cumin, oregano, paprika, cayenne pepper (if using), and tomato paste. Cook for 2 minutes.',
+        '5. Add crushed tomatoes, beef stock, kidney beans, and optional ingredients (corn kernels and carrots). Season with salt and pepper.',
+        '6. Bring to a simmer over low heat. Cover and cook for about 1 hour, stirring occasionally.',
+        '7. Adjust seasoning if needed. Serve hot.'
+      ],
+      prepTime: '20 minutes',
+      cookTime: '90 minutes',
+      totalTime: '110 minutes',
+    },
+    {
+      id: 2,
+      name: 'PENNE WITH SALMON and Basil & Tomatoes',
+      description: 'Experience the perfect blend of zesty lemon and aromatic garlic with this roasted chicken recipe',
+      image: menu8Image.src,
+      time: '1 HOUR',
+      difficulty: 'HARD PREP',
+      servings: '4 SERVES',
+      ingredients: [
+        '1 lb salmon filet',
+        '2 tbsp Italian seasoning',
+        'Salt and pepper',
+        '1 tbsp lemon juice',
+        '500g tomatoes',
+        '1 cup chopped basil leaves',
+        '2 garlic cloves, chopped',
+        'Olive oil',
+        '1 tbsp butter',
+        'Optional: 1/2 cup heavy cream',
+        'Optional: Grated parmesan'
+      ],
+      prepHacks: [
+        'Cook half of the salmon and penne in advance.',
+        'Swap in cooked items at end of recipe.',
+      ],
+      NoorTips:[
+       ' No one likes mushy pasta – it is better to under cook it a bit. Then you can add water in step 5 if needed and simmer it all together for a little longer.',
+        'My favorite seasonal tomato is ‘early girl’ tomatoes, but you can also use cherry or roma tomatoes.',
+       ' Throwing pasta at a wall to see if it sticks to see if it’s done is a myth, but it is still fun to do!',
+        'See who can get it to stick the highest!',
+        'Watch out for the bones when flaking the salmon.',
+        'Adventurer Tip: Add different spices to the salmon when seasoning it. You can try dill, chilli flakes, honey, maple syrup, or even tandoori spice!',
+        
+  
+      ],
+      instructions: [
+        `1. Season salmon filet with olive oil, salt, pepper, Italian seasoning, and lemon juice. Bake in a pre-heated oven at 425°F for 25 minutes.`,
+        `2. Cook penne pasta until desired tenderness and set aside.`,
+        `3. In a large saucepan, heat olive oil and sauté chopped garlic for 1 minute.`,
+        `4. Add tomatoes (chopped to desired size) and heat gently, avoiding over-liquefying.`,
+        `5. Flake cooked salmon into small pieces and add to tomatoes along with penne, butter, and basil.`,
+        `6. Optional: Stir in heavy cream before adding basil and simmer for 1 minute. Top with grated parmesan.`,
+      ],
+      prepTime: '10 minutes',
+      cookTime: '30 minutes',
+      totalTime: '40 minutes',
+    },
+    {
+      id: 3,
+      name: 'MATAR KHEEMA (Peas and Meat)',
+      description: 'Aromatic and flavorful beef curry with a blend of spices and vegetables',
+      image: menu9Image.src,
+      time: '40 MIN',
+      difficulty: 'MEDIUM PREP',
+      servings: '4 SERVES',
+      ingredients: [
+        '1 lb ground beef',
+        '1 tbsp white vinegar',
+        'Salt',
+        '3-4 tbsp oil',
+        '2 whole cloves',
+        '2 whole black peppercorns',
+        '3-4 whole cardamom pods',
+        '2 small cinnamon sticks',
+        '1 tsp cumin seeds',
+        'Optional: 2 curry leaves',
+        '1 diced medium onion',
+        '1/2 can chopped tomatoes',
+        '1.5 tsp tomato sauce',
+        '1/2 tsp crushed garlic',
+        '1 tsp crushed ginger',
+        '2 tbsp yogurt',
+        '1/4 tsp turmeric',
+        '1/2 tsp cumin powder',
+        '2 cups peas (frozen is okay)',
+        '2 medium potatoes (1” cubes)',
+        '1/2 tsp garam masala',
+        '2 tbsp lemon juice'
+      ],
+      prepHacks: [
+        'Cook beef and onions in advance.',
+      ],
+      NoorTips:[
+  
+       ' Dont forget ‘Mise en place’! Mix the ingredients for each step together beforehand.',
+        'Did you know: Keema Matar is a dish made popular during Mugal India times! It is a royal treat!',
+        'Don’t forget to wear your crown when eating it! Over the years, it has had many regional variaties in Pakistan, Gujarat, Bangladesh, and this East African influenced variety (where the order of words in the name are swapped too!).',
+       ' Dada’s joke: What did the pea say to the other pea? Don’t mutter. :)',
+  
+      ],
+      instructions: [
+        ' 1. Cook ground beef with salt and white vinegar until no longer pink. Break up clumps as it cooks.',
+        ' 2. Remove beef from the pot and set aside. Heat oil in the same pot and add whole spices (cloves, peppercorns, cardamom, cinnamon sticks, cumin seeds). Heat for 2 minutes until aromatic.',
+        ' 3. Add diced onions to the pot and sauté until golden brown.',
+        ' 4. Stir in potatoes, peas, and 1 cup of water. Simmer until potatoes and peas are tender.',
+        '5.  Add garam masala, lemon juice, and optionally heavy cream. Simmer for another minute.',
+        ' 6. Serve hot with rice or bread.',
+      ],
+      prepTime: '15 minutes',
+      cookTime: '25 minutes',
+      totalTime: '40 minutes',
+    },
+    {
+      id: 4,
+      name: 'WIENER SCHNITZEL',
+      description: 'Crispy and tender veal cutlets coated with breadcrumbs, perfect with lemon wedges.',
+      image: menu10Image.src,
+      time: '30 MIN',
+      difficulty: 'EASY PREP',
+      servings: '4 SERVES',
+      ingredients: [
+        '1 lb veal (cut into 4 pieces)',
+        '2 eggs',
+        '1/2 cup all-purpose flour',
+        '1/2 cup breadcrumbs',
+        'Lemon wedges',
+        '1/2 tsp salt',
+        'Oil, butter, or ghee for frying'
+      ],
+      prepHacks: [
+        '',
+        '',
+      ], 
+      NoorTips:[
+  
+        'Like always - have fun with breaking the eggs!',
+       ' Practice ‘dredging’. This is the technique of coating the cutlets. Use one hand for when the veal is dry, and the other for when it is wet with the egg.',
+        'Don’t press the breadcrumbs into the veal too hard - you don’t want them soggy before cooking. Think of it as getting the breadcrumbs to coat the eggs, not the veal directly.',
+        'You can add more seasoning to the flour or eggs – herbs, paprika, parsley.',
+       ' Fun Tip: Cut the veal into different shapes like the moon, a star, or even a heart!',
+       ' Did you know: ‘Wiener’ means ‘of Vienna’ in German, and must use veal in Austria, but you can use other meats too. Chicken is also yummy!',
+  
+      ],
+      instructions: [
+        ' 1. Place veal pieces into a ziplock bag or between plastic wrap and gently pound with a mallet or pestle until 1/4” thick or less.',
+        ' 2. Prepare three shallow bowls for coating: one with flour and salt, one with beaten eggs, and one with breadcrumbs.',
+        '3.  Coat each veal piece first in the flour mixture, shaking off excess.',
+        '4.  Dip into the beaten eggs, allowing excess to drip off.',
+        '5.  Roll the veal in breadcrumbs, ensuring an even coating without pressing too hard.',
+        '6.  Heat oil, butter, or ghee in a skillet over medium- high heat.Fry veal cutlets for 2 - 3 minutes on each side until golden brown and cooked through.',
+        '7.  Serve hot with lemon wedges.',
+      ],
+      prepTime: '15 minutes',
+      cookTime: '15 minutes',
+      totalTime: '30 minutes',
+    },
+  ];
+  
