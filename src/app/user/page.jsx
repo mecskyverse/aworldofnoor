1a84453f5981cb2c99ff2d860f09779eeabc170c
Aakash <vermaaakash1000@gmail.com>
2024-07-19 23:43:48 +0530

    user page updated

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,33 +1,8 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { redirect } from 'next/navigation'
-import { createNewClient } from '@/utils/supabase/client'
 
-
-const getUserDetail = async (setIsLoggedIn) => {
-    const supabase = createNewClient()
-    const {
-        data: { user },
-        error
-    } = await supabase.auth.getUser()
-    if (error || !user) {
-        redirect('/signin')
-    }
-    else {
-        setIsLoggedIn(true)
-    }
-}
-
-export default function page() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    useEffect(() => {
-        getUserDetail(setIsLoggedIn)
-    }, [])
+export default async function page() {
 
     return (
         <div className='min-h-screen'>User Section</div>
     )
-
-
 }
 
