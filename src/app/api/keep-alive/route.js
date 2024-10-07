c6fc1bce98d335d24e13757df8cf388338b98e0e
Aakash <vermaaakash1000@gmail.com>
2024-10-07 12:30:56 +0530

    node-cron removed

--- src/app/api/keep-alive/route.js
+++ src/app/api/keep-alive/route.js
@@ -1,28 +1,15 @@
 import { NextResponse } from "next/server";
 import { createNewClient } from '@/utils/supabase/client';
 const supabase = createNewClient()
-var cron = require('node-cron');
 
 export async function GET() {
     try {
-        cron.schedule('* * * * *', async () => {
-
-            console.log('')
-            console.log('######################################')
-            console.log('#                                    #')
-            console.log('# Running scheduler every 20 minutes #')
-            console.log('#                                    #')
-            console.log('######################################')
-            console.log('')
-
-            // Perform your action here
-            const { data, error } = await supabase
+        const { data, error } = await supabase
         .from('user_images')
         .select('id')
         .limit(1)
         if (error) throw error
         return NextResponse.json({ message: 'Keep-alive successful' })
-        });
     } catch (error) {
         console.error('Keep-alive failed:', error)
         return NextResponse.json({ error: 'Keep-alive failed' })
